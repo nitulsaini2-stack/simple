da7582bea7231d3e8a4fea60f3e6e86bc192cd56
Ghanshyam Kumar <ghanshyam@91astrology.com>
2026-05-12 15:39:31 +0530

    Gym project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//     images: {
+//     domains: ["images.unsplash.com"],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
